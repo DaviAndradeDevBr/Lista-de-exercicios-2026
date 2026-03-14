1c80522168b6d7a9cc9394d5bd3e1de25a6a4ee7
Davi Andrade <contatodaviandrade20@gmail.com>
2026-03-14 15:57:55 -0300

    Adicionado Exercicio04

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,28 @@
+﻿// 4. Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+// F= (C * 1.8) + 32
+
+while (true)
+{
+    Console.Clear();
+
+    Console.Write("Digite a temperatura em Celsius: ");
+    decimal Celsius = Convert.ToDecimal(Console.ReadLine());
+
+    // Formula: (C * 1.8) + 32
+    decimal fahrenheit = (Celsius * 1.8m) + 32;
+
+    Console.WriteLine($"A temperatura de {Celsius}°C equivale a: {fahrenheit}°F");
+
+    Console.WriteLine("\nPressione ENTER para continuar...");
+    Console.ReadLine();
+
+    Console.Write("Deseja continuar? (S/N): ");
+    string? resposta = Console.ReadLine();
+
+    if (resposta == "n" || resposta == "N")
+    {
+        break;
+    }
+}
+
+
