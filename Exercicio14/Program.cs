c70daf78b901572ed4a8076eb938507fd39dee25
Davi Andrade <contatodaviandrade20@gmail.com>
2026-03-17 23:57:56 -0300

    Adicionado Exercicio14

---
+++ Exercicio14/Program.cs
@@ -0,0 +1,41 @@
+﻿// 14. Escreva um algoritmo que leia três valores inteiros e diferentes e mostre-os em ordem decrescente.
+
+
+using System.Collections.Generic;
+using System.Linq;
+
+while (true)
+{
+    Console.Clear();
+    
+    // Criamos uma lista para guardar os números
+    List<int> numeros = new List<int>();
+
+    Console.Write("Digite o 1º valor inteiro: ");
+    numeros.Add(Convert.ToInt32(Console.ReadLine()));
+
+    Console.Write("Digite o 2º valor inteiro: ");
+    numeros.Add(Convert.ToInt32(Console.ReadLine()));
+
+    Console.Write("Digite o 3º valor inteiro: ");
+    numeros.Add(Convert.ToInt32(Console.ReadLine()));
+
+    // Ordena a lista (Crescente) e depois inverte para ficar Decrescente
+    numeros.Sort();
+    numeros.Reverse();
+
+    Console.WriteLine("\nOs números em ordem decrescente são: ");
+    Console.WriteLine(string.Join(" - ", numeros));
+
+    Console.WriteLine("\nPressione ENTER para continuar...");
+    Console.ReadLine();
+
+    Console.Write("Deseja continuar? (S/N): ");
+    string? resposta = Console.ReadLine();
+
+    if (resposta == "n" || resposta == "N")
+    {
+        break;
+    }
+
+}
